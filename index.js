7ed5716598d9efbce010349358cc60f146dd11c8
Marcus Paulo <mpaulobr@gmail.com>
2022-08-29 19:47:17 +0100

    ajust express

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-const yargs = require('yargs');
-const axios = require('axios').default;
+const axios = require('axios').default; // Import do Axios
 
-const user = 'marcuspaulo';
+const user = 'marcuspaulo'; // Apenas uma variável para teste, vamos mudar no futuro
 
-if (user) {
+if (user) { // Apenas para verificar se o valor não está vazio, podemos adicionar um else
     let url = `https://api.github.com/users/${user}/repos`
-    axios.get(url, {
-
-    })
-        .then((response) => {
-            for (let { full_name, url } of response.data) {
-                console.log(`full_name: ${full_name} - url ${url}`);
+    axios.get(url, {})
+        .then((response) => {  // Uma chamada para a API
+            for (let { full_name, url } of response.data) { // Após a resposta, vamos percorrer todos os elementos
+                console.log(`full_name: ${full_name} - url ${url}`); // Dessa API, eu quero apenas o Nome e a URL dos repositórios
             }
         })
         .catch(err => {
-            console.log(err);
+            console.log(err); // Só vai entrar aqui, em caso de erro da API.
         })
 }
